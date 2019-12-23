a2762e9efbb3008a950b48b4091c6638ff36d2c8
joellobo <joellobo@gmail.com>
2019-12-23 10:07:46 -0300

    refactoring

--- expressjs/app1/index.js
+++ expressjs/app1/index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const CJSON = 'application/json'
+const CXML = 'text/xml'
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('*', function(req, res){
-    res.set('Content-Type', 'text/json')
-    const msg = {
-        message: 'what'
+    let msg = '';
+
+    const acceptJSON = req.accepts(CJSON)
+    console.log(`accept:${acceptJSON}`)
+
+    const acceptXML = req.accepts(CXML)
+    console.log(`accept:${acceptXML}`)
+
+    if (acceptJSON === CJSON) {
+        res.set('Content-Type', CJSON)
+        msg = {
+            message: 'what'
+        }
+    } 
+
+    if (acceptXML === CXML) {
+        res.set('Content-Type', CXML)
+        msg = '<xml>Error 404</xml>'
     }
     res.status(404).send(msg)
 })
